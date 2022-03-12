cc4c7addc38ce31e98c16282c8c0d4bce33e4016
Jeremy Louie <jeremysf74@yahoo.com>
2022-03-12 11:38:13 -0800

    Added skeleton code for home page
    TO DO: fix node_modules being committed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Main from "./Main";
 
 const App: React.FC = () => {
+    useEffect(() => {
+        document.title = "Jeremy Louie"
+    }, [])
+
     return (
         <div className="App">
             <Main />
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,32 @@
 
 import React from "react";
+import "./home.css"
+import * as Unicons from "@iconscout/react-unicons";
+
+const icon_size = 32.5;
 
 const Home: React.FC = () => {
     return (
-        <div>
-            <h1>Home</h1>
-            <p>Home page content</p>
+        <div className="landing_page_wrapper">
+            <div className="greetings_wrapper">
+                <div className="spine" />
+                <h2>Hello! My name is</h2>
+                <h1><a href="https://www.linkedin.com/in/jeremylouie98/" target="_blank" rel="noreferrer">Jeremy Louie</a></h1>
+                <p>I'm currently building software needed to detect water contaminants in refillable water stations at <a href="https://atlas-scientific.com/" target="_blank" rel="noreferrer">Atlas Scientific</a></p>
+                <div className="icon_wrapper">
+                    <a href="https://www.linkedin.com/in/jeremylouie98/" target="_blank" rel="noreferrer">
+                        <Unicons.UilLinkedin size={icon_size} className="icon" />                    
+                    </a>
+
+                    <a href="https://github.com/jsethlui" target="_blank" rel="noreferrer">
+                        <Unicons.UilGithub size={icon_size} className="icon" />    
+                    </a>
+
+                    <a href="#about">
+                        <Unicons.UilInfoCircle size={icon_size} className="icon" />   
+                    </a>
+                </div>
+            </div>
         </div>
     );
 };
