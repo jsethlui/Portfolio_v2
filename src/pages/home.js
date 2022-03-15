928c8e1feab03c5dcd74e70b1b944eda808117ca
Jeremy Louie <jeremysf74@yahoo.com>
2022-03-15 00:48:04 -0700

    Some changes to about section

--- src/pages/home.js
+++ src/pages/home.js
@@ -17,6 +17,10 @@ const Home: React.FC = () => {
                     <h2>Hello! My name is</h2>
                     <h1><a href="https://www.linkedin.com/in/jeremylouie98/" target="_blank" rel="noreferrer">Jeremy Louie</a></h1>
                     <p>I'm currently building software needed to detect water contaminants in refillable water stations at <a href="https://atlas-scientific.com/" target="_blank" rel="noreferrer" className="atlas_bold">Atlas Scientific</a></p>
+
+                    {/*add momentum inspired thingy*/}
+                    {/*i.e. right now i'm learning x, y, z,...*/}
+
                     <div className="icon_wrapper">
                         {/*LinkedIn nkedin*/}
                         <a href="https://www.linkedin.com/in/jeremylouie98/" target="_blank" rel="noreferrer" title="LinkedIn">
@@ -38,7 +42,27 @@ const Home: React.FC = () => {
 
             {/* about me */}
             <div className="content_wrapper">
-                <h1>About Me</h1>
+                <div className="surrounding_wrapper">
+
+                    {/*skills*/}
+                    <div className="skills_wrapper">
+                        <div className="cell">
+                            <h3>Languages{"\n"}Learned</h3>
+                            <p>6</p>
+                        </div>
+                    </div>
+
+                    {/*avatar*/}
+                    <div className="avatar_wrapper">
+                    </div>
+
+                    {/*projects*/}
+                    <div className="projects_wrapper">
+                    </div>
+                </div>
+                <div className="icon_wrapper">
+                </div>
+                <p>about me</p>
             </div>
         </div>
     );
