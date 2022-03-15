47b5ffade5d850dc5e9dba21edcaf506846f17dc
Jeremy Louie <jeremysf74@yahoo.com>
2022-03-15 14:05:51 -0700

    Added sidebars for about page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,10 @@
 
 import React from "react";
 import "./home.css";
-import { UilLinkedin, UilGithub, UilInfoCircle } from "@iconscout/react-unicons";
+import { UilLinkedin, UilGithub, UilInfoCircle, UilEnvelope } from "@iconscout/react-unicons";
 import Menu from "./menu";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 const Home: React.FC = () => {
     const iconSize = 32.5;
@@ -17,10 +19,6 @@ const Home: React.FC = () => {
                     <h2>Hello! My name is</h2>
                     <h1><a href="https://www.linkedin.com/in/jeremylouie98/" target="_blank" rel="noreferrer">Jeremy Louie</a></h1>
                     <p>I'm currently building software needed to detect water contaminants in refillable water stations at <a href="https://atlas-scientific.com/" target="_blank" rel="noreferrer" className="atlas_bold">Atlas Scientific</a></p>
-
-                    {/*add momentum inspired thingy*/}
-                    {/*i.e. right now i'm learning x, y, z,...*/}
-
                     <div className="icon_wrapper">
                         {/*LinkedIn nkedin*/}
                         <a href="https://www.linkedin.com/in/jeremylouie98/" target="_blank" rel="noreferrer" title="LinkedIn">
@@ -44,25 +42,53 @@ const Home: React.FC = () => {
             <div className="content_wrapper">
                 <div className="surrounding_wrapper">
 
-                    {/*skills*/}
-                    <div className="skills_wrapper">
-                        <div className="cell">
-                            <h3>Languages{"\n"}Learned</h3>
+                    {/* skills */}
+                    <Row className="cell_wrapper" style={{right: "0"}}>
+                        <Col className="cell_right">
+                            <h3>Number of Internships</h3>
+                            <p>2</p>
+                        </Col>
+
+                        <Col className="cell_right">
+                           <h3>Projects Completed</h3>
+                            <p>3</p>
+                        </Col>
+
+                        <Col className="cell_right">
+                            <h3>Languages Learned</h3>
                             <p>6</p>
-                        </div>
-                    </div>
+                        </Col>
+                    </Row>
 
                     {/*avatar*/}
                     <div className="avatar_wrapper">
                     </div>
 
-                    {/*projects*/}
-                    <div className="projects_wrapper">
-                    </div>
+                    <Row className="cell_wrapper" style={{left: "0"}}>
+                        <Col className="cell_left">
+                            <h3>Contact Info</h3>
+                            <ul>
+                                <li>San Francisco, CA</li>
+                                <li>
+                                    <a href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=jsethlui@.ucla.edu" target="_blank" rel="noreferrer">
+                                        <UilEnvelope />
+                                    </a>
+                                </li>
+                            </ul>
+                        </Col> 
+    
+                         <Col className="cell_left">
+                            <h3>Interests</h3>
+                            <ul>
+                                <li>Software Engineering</li>
+                                <li>Cloud Computing</li>
+                                <li>UI / UX Design</li>
+                            </ul>
+                        </Col> 
+
+                    </Row>
                 </div>
-                <div className="icon_wrapper">
-                </div>
-                <p>about me</p>
+                {/*<p>about me</p>*/}
             </div>
         </div>
     );
