aae47391fdbc1f60c8ed5cc394521bfe3f971e9e
Jeremy Louie <jeremysf74@yahoo.com>
2022-03-19 19:04:37 -0700

    Added about me section

--- src/pages/components/About.js
+++ src/pages/components/About.js
@@ -7,52 +7,73 @@ import Avatar from "../../assets/avatar.jpg"
 
 const About: React.FC = () => {
     return (
-        <div className="about_wrapper" id="about">
-            {/* skills */}
-            <Row className="cell_wrapper" style={{right: "0"}}>
-                <Col className="cell_right">
-                    <h3>Research<br />Positions</h3>
-                    <p>2</p>
-                </Col>
+        <div className="surround_wrapper">
+            <div className="stats_wrapper">
+                {/* skills */}
+                <Row className="cell_wrapper" style={{right: "0"}}>
+                    <Col className="cell_right">
+                        <h3>Research<br />Positions</h3>
+                        <p>2</p>
+                    </Col>
 
-                <Col className="cell_right">
-                   <h3>Projects<br />Completed</h3>
-                    <p>3</p>
-                </Col>
+                    <Col className="cell_right">
+                       <h3>Projects<br />Completed</h3>
+                        <p>3</p>
+                    </Col>
 
-                <Col className="cell_right">
-                    <h3>Honors<br />Completed</h3>
-                    <p>5</p>
-                </Col>
-            </Row>
+                    <Col className="cell_right">
+                        <h3>Honors<br />Completed</h3>
+                        <p>5</p>
+                    </Col>
+                </Row>
 
-            <Row className="avatar_wrapper">
-                <img src={Avatar} alt="avatar" />
-            </Row>
+                <Row className="avatar_wrapper">
+                    <img src={Avatar} alt="avatar" />
+                </Row>
 
-            <Row className="cell_wrapper" style={{left: "0"}}>
-                <Col className="cell_left">
-                    <h3>Education</h3>
-                    <ul>
-                        <li>University of California, Los Angeles</li>
-                        <li>City College of San Francisco</li>
-                    </ul>
-                </Col> 
+                <Row className="cell_wrapper" style={{left: "0"}}>
+                    <Col className="cell_left">
+                        <h3>Education</h3>
+                        <ul>
+                            <li>University of California, Los Angeles</li>
+                        </ul>
+                    </Col> 
 
-                <Col className="cell_left">
-                    <h3>Location</h3>
-                    <p>San Francisco</p>
-                </Col> 
+                    <Col className="cell_left">
+                        <h3>Location</h3>
+                        <p>San Francisco</p>
+                    </Col> 
 
-                <Col className="cell_left">
-                    <h3>Interests</h3>
-                    <ul>
-                        <li>Software Engineering</li>
-                        <li>Cloud Computing</li>
-                        <li>UI / UX Design</li>
-                    </ul>
-                </Col> 
-            </Row>
+                    <Col className="cell_left">
+                        <h3>Interests</h3>
+                        <ul>
+                            <li>Software Engineering</li>
+                            <li>Cloud Computing</li>
+                        </ul>
+                    </Col> 
+                </Row>
+            </div>
+
+            <div className="about_me_wrapper">
+                <h1>About Me</h1>
+                <p>
+                    My name is Jeremy, and I enjoy developing <b className="bold">useful products</b> with
+                    a digital eye! Currently, I'm currently a Software Engineer for <b className="bold">Atlas Scientific</b>. 
+                    More specifically, I write <b className="bold">Python</b> programs that have ported over our data (collected from a Raspberry Pi)
+                    onto <b className="bold">Google Cloud's Firestone</b> database, and automating our data
+                    collection procedures using common <b className="bold">design patterns</b>!
+
+                    <br /><br />
+
+                    While programming has given me many sleepless nights, it also give rise to
+                    my <b className="bold">coffee</b> addiction! In fact, I love exploring different cities and their trying unique coffee flavor
+                    profiles. 
+
+                    <br /><br />
+
+                    Above all, I’m always willing to keep up with the rapidly changing pace of technology by <b className="bold">learning</b> something new everyday
+                </p>
+            </div>
         </div>
     );
 };
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,23 +2,18 @@
 import React from "react";
 import "./home.css";
 import "./scrollbar.css"
-import { UilLinkedin, 
-         UilGithub,
-         UilInfoCircle } from "@iconscout/react-unicons";
 import Intro from "./components/Intro.js"
 import About from "./components/About.js"
-import Footer from "./components/Footer.js"
 import Menu from "./menu";
 
 const Home: React.FC = () => {
-    const iconSize = 32.5;
-
     return (
         <div>
             <Menu />
             <Intro />
+            
+            <div id="about" />
             <About />
-            {/*<Footer />*/}
         </div>
     );
 };
