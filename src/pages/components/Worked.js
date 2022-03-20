c889c32b75d57fbea249121b28bf3ccd2ac2897c
Jeremy Louie <jeremysf74@yahoo.com>
2022-03-20 13:09:52 -0700

    Added vertical styling to React tab

--- src/pages/components/Worked.js
+++ src/pages/components/Worked.js
@@ -1,11 +1,36 @@
 
 import React from "react";
 import "./Worked.css"
+import { Tab,
+         Tabs,
+         TabList,
+         TabPanel } from "react-tabs";
 
 const Worked: React.FC = () => {
     return (
-        <div>
-            <p>some test</p>
+        <div className="tabs_wrapper">
+            <Tabs>
+                <TabList>
+                    <Tab>Atlas Scientific</Tab>
+                    <Tab>Pocket Racers</Tab>
+                    <Tab>Juni Learning</Tab>
+                </TabList>
+
+                {/* atlas scientific */}
+                <TabPanel>
+                    <h2>atlas scientific</h2>
+                </TabPanel>
+
+                {/* pocket racers */}
+                <TabPanel>
+                    <h2>pocket racers</h2>
+                </TabPanel>
+
+                {/* juni learning */}
+                <TabPanel>
+                    <h2>juni learning</h2>
+                </TabPanel>                
+            </Tabs>
         </div>
     );
 };
