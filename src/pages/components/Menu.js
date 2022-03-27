07b956abf6ff5c0d54fd8427d7eaf49fbc38f6ac
Jeremy Louie <jeremysf74@yahoo.com>
2022-03-26 18:25:48 -0700

    Added tabs and styling for menu bar

--- src/pages/components/Menu.js
+++ src/pages/components/Menu.js
@@ -1,18 +1,29 @@
 
-import { React, useState } from "react";
+import { React, useState, useRef } from "react";
 import "./Menu.css";
 import { Slant as Hamburger } from 'hamburger-react'
 
 const Menu: React.FC = () => {
     const menuSize = 35;
+
+    const dropDownRef = useRef(null);
     const [isOpen, setOpen] = useState(false);
     const handleToggle = () => {
         setOpen(!isOpen)
     };
 
     return (
-        <div className="menu_wrapper">
-            <Hamburger size={menuSize} distance="sm" onClick={handleToggle} rounded />
+        <div className="menu_wrapper" onClick={handleToggle}>
+            <Hamburger size={menuSize} distance="sm" toggled={isOpen} rounded />
+            <nav ref={dropDownRef} className={`menu ${isOpen ? 'active' : 'inactive'}`}>
+                <ul>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#worked">Experience</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="contact">Contact</a></li>
+                </ul>
+            </nav>
+
         </div>
     );
 };
