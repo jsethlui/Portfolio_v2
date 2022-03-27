93278826ec0247b4eeb5c2fafb8f580f03226e2d
Jeremy Louie <jeremysf74@yahoo.com>
2022-03-27 12:50:48 -0700

    Added some styling for projectsn

--- src/pages/components/Projects.js
+++ src/pages/components/Projects.js
@@ -1,10 +1,11 @@
 
 import React from "react"
 import "./Projects.css"
-import Col from "react-bootstrap/Col"
-import Row from "react-bootstrap/Row"
+import Image from "react-bootstrap/Image"
 import zoomieRoomies from "../../assets/zoomie_roomies.png"
 import rungeKutta from "../../assets/runge_kutta.png"
+import { UilGithub,
+         UilExternalLinkAlt } from "@iconscout/react-unicons"
 
 const Projects: React.FC = () => {
     return (
@@ -16,7 +17,42 @@ const Projects: React.FC = () => {
                 <p>Research</p>
             </div>
             <div className="cards_wrapper">
-                
+                <Image src={zoomieRoomies} responsive />
+                <div className="about_card_wrapper">
+                    <h3><a href="https://github.com/cs130-w22/Group-B1" target="_blank" rel="noreferrer">Zoomie Roomies</a></h3>
+
+                    {/*roles*/}
+                    <p>
+                        <b className="bold">Roles:</b> Front-end Developer & Design
+                    </p>
+
+                    {/*abstract*/}
+                    <p>
+                        <b className="bold">Abstract:</b> we developed a web app
+                        platform aimed at making roommate searching easier for
+                        post-graduate students
+                    </p>
+
+                    {/*brief*/}
+                    <p>                    
+                        <b className="bold">Brief:</b> during post-graduation, the most difficult task
+                        when searching for optimal roommates is being matched with one (or more) of 
+                        similar interests. Currently, students must advertise themselves on platforms
+                        such as Facebook or by "word of ear." Very few platforms exist that are
+                        targetted at matching post-graduate roommates together.
+                        <br /><br />
+
+                        As part of our capstone project for COM SCI 130: Software Engineering, we aim
+                        at making roommate searching easier for post-graduate students with Zoomie
+                        Roomies.
+                    </p>
+                    <ul className="project_tools_wrapper">
+                        <li>Agile</li>
+                        <li>React</li>
+                        <li>Git</li>
+                        <li>CSS and Javascript</li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
