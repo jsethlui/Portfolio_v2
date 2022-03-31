3e9fadf01c53221e7aba0263a583eb180e9961b3
Jeremy Louie <jeremysf74@yahoo.com>
2022-03-31 15:05:07 -0700

    Added skeleton code for projects wrapper

--- src/pages/components/Projects.js
+++ src/pages/components/Projects.js
@@ -1,36 +1,64 @@
 
-import React from "react"
+import { React, useState, useRef } from "react"
 import "./Projects.css"
 import Image from "react-bootstrap/Image"
 import zoomieRoomies from "../../assets/zoomie_roomies.png"
 import rungeKutta from "../../assets/runge_kutta.png"
 import { UilGithub,
          UilExternalLinkAlt } from "@iconscout/react-unicons"
+import { Tab,
+         Tabs,
+         TabList,
+         TabPanel } from "react-tabs"
 
 const Projects: React.FC = () => {
     const iconSize = 25;
+
+    const imageRef = useRef(null);
+    const [zoomieRoomieOpen, setZoomieRoomieOpen] = useState(false);
+    const handleToggle = () => {
+        setZoomieRoomieOpen(!zoomieRoomieOpen)
+    };
     return (
-        <div className="projects_wrapper">
-            <h1>My Projects</h1>
+        <div className="projects_wrapper" onClick={handleToggle}>
+            <div className="image_card_wrapper" ref={imageRef} className={`image_card_wrapper ${zoomieRoomieOpen ? 'active' : 'inactive'}`}>
+                
+            </div>
+            <Tabs style={{display: "flex"}}>
+                <TabList>
+                    <Tab toggled={setZoomieRoomieOpen}>Zoomie Roomies</Tab>
+                    <Tab toggled={setZoomieRoomieOpen}>Runge Kutta Analysis</Tab>
+                </TabList>
+
+                <TabPanel>
+                    <div>
+                        <p>zoomie roomies</p>
+                    </div>
+                </TabPanel>
+
+                <TabPanel>
+                    <div>
+                        <p>runge kutta</p>
+                    </div>
+                </TabPanel>
+            </Tabs>
+{/*            <h1>My Projects</h1>
             <div className="skills_wrapper">
                 <p>Dev Experience</p>
                 <p className="bullet">•</p>
                 <p>Research</p>
             </div>
 
-            {/*zoomie roomies*/}
             <div className="cards_wrapper">
                 <div className="image_wrapper">
                     <a href="https://use-zoomie-roomies.herokuapp.com/" target="_blank" rel="noreferrer" title="Zoomie Roomies" className="animated_link"> 
                         <Image src={zoomieRoomies} responsive />
                     </a>
                     <div>
-                        {/*github*/}
                         <a href="https://github.com/cs130-w22/Group-B1" target="_blank" rel="noreferrer" className="link" title="GitHub Repo">
                             <UilGithub size={iconSize} />
                         </a>
 
-                        {/*zoomie roomie link*/}
                         <a href="https://use-zoomie-roomies.herokuapp.com/" target="_blank" rel="noreferrer" className="link" title="Zoomie Roomies"> 
                             <UilExternalLinkAlt size={iconSize} />
                         </a>
@@ -39,19 +67,16 @@ const Projects: React.FC = () => {
                 <div className="about_card_wrapper">
                     <h3><a href="https://github.com/cs130-w22/Group-B1" target="_blank" rel="noreferrer" title="Zoomie Roomies">Zoomie Roomies</a></h3>
 
-                    {/*roles*/}
                     <p>
                         <b className="bold">Role:</b> Front-end Developer & Design
                     </p>
 
-                    {/*abstract*/}
                     <p>
                         <b className="bold">Abstract:</b> we developed a web app
                         platform aimed at making roommate searching easier for
                         post-graduate students
                     </p>
 
-                    {/*brief*/}
                     <p>                    
                         <b className="bold">Brief:</b> during post-graduation, the most difficult task
                         when searching for optimal roommates is being matched with one (or more) of 
@@ -76,20 +101,16 @@ const Projects: React.FC = () => {
                 </div>
             </div>
 
-
-            {/*runge kutta*/}
             <div className="cards_wrapper">
                 <div className="image_wrapper">
                     <a href="https://github.com/jsethlui/What-is-the-best-Runge-Kutta-Method-/blob/main/4th_Order_Runge_Kutta_Method_Analysis.ipynb" target="_blank" rel="noreferrer" title="Jupyter Notebook" className="animated_link">
                         <Image src={rungeKutta} responsive />
                     </a>
                     <div>
-                        {/*github*/}
                         <a href="https://github.com/jsethlui/What-is-the-best-Runge-Kutta-Method-" target="_blank" rel="noreferrer" className="link" title="GitHub Repo">
                             <UilGithub size={iconSize} />
                         </a>
 
-                        {/*notebook link*/}
                         <a href="https://github.com/jsethlui/What-is-the-best-Runge-Kutta-Method-/blob/main/4th_Order_Runge_Kutta_Method_Analysis.ipynb" target="_blank" rel="noreferrer" className="link" title="Jupyter Notebook"> 
                             <UilExternalLinkAlt size={iconSize} />
                         </a>
@@ -98,18 +119,15 @@ const Projects: React.FC = () => {
                 <div className="about_card_wrapper">
                     <h3><a href="https://github.com/jsethlui/What-is-the-best-Runge-Kutta-Method-/blob/main/4th_Order_Runge_Kutta_Method_Analysis.ipynb" target="_blank" rel="noreferrer" title="Jupyter Notebook">4th Order Runge Kutta Method Analysis</a></h3>
 
-                    {/*roles*/}
                     <p>
                         <b className="bold">Role:</b> Student Researcher
                     </p>
 
-                    {/*abstract*/}
                     <p>
                         <b className="bold">Abstract:</b> I apply the Runge Kutta method and its variation to approximate
                         two different ordinary differential equations around some given timestep
                     </p>
 
-                    {/*brief*/}
                     <p>                    
                         <b className="bold">Brief:</b> The Runge Kutta (RK) Method is an iterative time-step method
                         used to approximate ordinary differential equations (ODE's) around some fixed point. There 
@@ -138,7 +156,7 @@ const Projects: React.FC = () => {
                         <li>Jupyter Notebook</li>
                     </ul>
                 </div>
-            </div>
+            </div>*/}
         </div>
     );
 }
